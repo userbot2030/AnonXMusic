9bf85295df0877ad483daa1639044bc7fce39bfa
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-06 22:20:04 +0700

    Update config.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- config.py
+++ config.py
@@ -104,13 +104,13 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
 if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", Berlinmusic_support):
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
 if SUPPORT_CHAT:
-    if not re.match("(?:http|https)://", Berlinmusic_support):
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
