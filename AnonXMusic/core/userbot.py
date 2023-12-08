fae517b7848208a50c113039d3107b6e642eee3f
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-08 20:08:12 +0700

    Update userbot.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("Berlinmusic_support")
+                await self.one.join_chat("Asupan_bokepviral")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("Berlinmusic_support")
+                await self.two.join_chat("Asupan_bokepviral")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("Berlinmusic_support")
+                await self.three.join_chat("Asupan_bokepviral")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("Berlinmusic_support")
+                await self.four.join_chat("Asupan_bokepviral")
             except:
                 pass
             assistants.append(4)
