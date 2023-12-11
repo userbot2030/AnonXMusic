05e67271502758887526bd33e85a4d19dbef61a3
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-11 17:11:41 +0700

    Update queue.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/utils/inline/queue.py
+++ AnonXMusic/utils/inline/queue.py
@@ -66,8 +66,10 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="OWNER", user_id=f"OWNER_ID"),
-            InlineKeyboardButton(text="FOUNDER", url=f"https://t.me/midnightsexual"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
     ]
     return buttons
