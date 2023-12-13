ca6ee655058234243866d61e4fc12d4a3d7ff9ac
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-13 13:07:32 +0700

    Update start.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -27,11 +27,11 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=6851974966),
-            InlineKeyboardButton(text=_["S_B_2"], url=htts://t.me/Berlinsupportmanage),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"htts://t.me/Berlinsupportmanage"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=https://t.me/Asupan_bokepviral),
-            InlineKeyboardButton(text=_["S_B_7"], url=https://xnxx.com),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Asupan_bokepviral"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://xnxx.com"),
         ],
     ]
     return buttons
