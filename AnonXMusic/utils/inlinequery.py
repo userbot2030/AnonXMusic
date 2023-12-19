b68f3edf5407537cc1088b4359be0223c5425bbe
esanwtel <154304688+esanwtel@users.noreply.github.com>
2023-12-19 16:43:39 +0700

    Esa inlinequery.py
    Signed-off-by: esanwtel <154304688+esanwtel@users.noreply.github.com>

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴅᴀʜ ɢᴡ ᴊᴇᴅᴀ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ᴅᴀʜ ɢᴡ ʀᴇsᴜᴍᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"ᴅᴀʜ ɢᴡ sᴋɪᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴅᴀʜ ɢᴡ ᴇɴᴅ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="ᴅᴀʜ ɢᴡ sᴜғғʟᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ᴅᴀʜ ɢᴡ ʟᴏᴏᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+            thumb_url="https://graph.org/file/0a846c1065661586a8c4b.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
