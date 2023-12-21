77b77b99f592c8e4116d71063668401d731c7c24
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-22 06:11:23 +0700

    T queue.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -156,7 +156,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
     basic[videoid] = False
     buttons = queue_back_markup(_, what)
     med = InputMediaPhoto(
-        media="https://graph.org/file/6c4f56e9091d0c772a193.jpg",
+        media="https://graph.org/file/5d141cc6ee9d131830dea.jpg",
         caption=_["queue_1"],
     )
     await CallbackQuery.edit_message_media(media=med)
