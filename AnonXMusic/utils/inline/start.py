558a7cb2a4e885c9b5309f0b540f4dcc707bbd4c
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-23 23:25:20 +0700

    NT start.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/asupanmidnight"
+                text=_["S_B_1"], url=f"https://t.me/Asupan_viral18plus"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
