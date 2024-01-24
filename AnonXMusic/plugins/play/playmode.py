f7b2aeab0850c5cf73bc5352e5609b1e2668b866
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2024-01-24 14:40:18 +0700

    Update playmode.py
    Signed-off-by: kansya-nt <149471959+kansya-nt@users.noreply.github.com>

--- AnonXMusic/plugins/play/playmode.py
+++ AnonXMusic/plugins/play/playmode.py
@@ -18,14 +18,14 @@ async def playmode_(client, message: Message, _):
         Direct = None
     is_non_admin = await is_nonadmin_chat(message.chat.id)
     if not is_non_admin:
-        Group = 
+        Group = False
     else:
-        Group = 
+        Group = False
     playty = await get_playtype(message.chat.id)
     if playty == "Everyone":
-        Playtype = 
+        Playtype = False
     else:
-        Playtype = 
+        Playtype = False
     buttons = playmode_users_markup(_, Direct, Group, Playtype)
     response = await message.reply_text(
         _["play_22"].format(message.chat.title),
